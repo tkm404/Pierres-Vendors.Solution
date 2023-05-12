0e3914c7a05091cd95d4ec7665c73f6dbfd1562e
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-12 11:43:56 -0700

    write test to determine if Vendor instance created properly. Add Dispose method to Vendor Tests.

---
+++ Pierres-Vendors.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PierresVendors.Models;
+using System;
+
+namespace PierresVendors.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test name", "test desc");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
