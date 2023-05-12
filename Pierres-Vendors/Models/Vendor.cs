5e8a545210addec9edba275d0c962282e1162e3f
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-12 11:39:34 -0700

    add way to add instance of Vendor to instances list.

--- Pierres-Vendors/Models/Vendor.cs
+++ Pierres-Vendors/Models/Vendor.cs
@@ -14,6 +14,7 @@ namespace PierresVendors.Models
     {
       Name = vendorName;
       Description = vendorDesc;
+      _instances.Add(this);
       Id = _instances.Count;
       Orders = new List<Order>{};
     }
