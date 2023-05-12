97d1c617c8c27de3cc33518069a7c3f9a052faf5
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-12 11:38:09 -0700

    add Vendor constructor, Vendor instances list, Clear method for instances.

--- Pierres-Vendors/Models/Order.cs
+++ Pierres-Vendors/Models/Order.cs
@@ -4,11 +4,11 @@ namespace PierresVendors.Models
 {
   public class Order
   {
-    public string Title {get; set;}
-    public string Description {get; set;}
-    public int Price {get; set;}
-    public string Date {get; set;}
-    private static DateTime _timeline {get; set;} = new DateTime() ;
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public string Date { get; set; }
+    private static DateTime _timeline { get; set; } = new DateTime();
     public Order(string title, string desc, int price)
     {
       Title = title;
--- Pierres-Vendors/Models/Vendor.cs
+++ Pierres-Vendors/Models/Vendor.cs
@@ -4,8 +4,23 @@ namespace PierresVendors.Models
 {
   public class Vendor
   {
-    public string Name {get; set;}
-    public string Description {get; set;}
-    public List<Order> Orders {get; set;}
+    private static List<Vendor> _instances = new List<Vendor> { };
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+
+    public Vendor(string vendorName, string vendorDesc)
+    {
+      Name = vendorName;
+      Description = vendorDesc;
+      Id = _instances.Count;
+      Orders = new List<Order>{};
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
