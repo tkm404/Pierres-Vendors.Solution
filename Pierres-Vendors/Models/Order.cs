f9bb67b43b4d5c1c3c1303d5c6b94f3ba617f8f8
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-12 08:46:11 -0700

    add Title, Description, Price, and Date fields to Order class.

--- Pierres-Vendors/Models/Order.cs
+++ Pierres-Vendors/Models/Order.cs
@@ -2,6 +2,9 @@ namespace PierresVendors.Models
 {
   public class Order
   {
-    
+    public string Title {get; set;}
+    public string Description {get; set;}
+    public int Price {get; set;}
+    public string Date {get; set;}
   }
 }
