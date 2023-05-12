5685dca4511e6bccf30cf570988973490941d7a0
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-12 09:00:03 -0700

    add constructor field to Order class.

--- Pierres-Vendors/Models/Order.cs
+++ Pierres-Vendors/Models/Order.cs
@@ -6,5 +6,12 @@ namespace PierresVendors.Models
     public string Description {get; set;}
     public int Price {get; set;}
     public string Date {get; set;}
+    public Order(string title, string desc, int price, string date)
+    {
+      Title = title;
+      Description = desc;
+      Price = price;
+      Date = date;
+    }
   }
 }
