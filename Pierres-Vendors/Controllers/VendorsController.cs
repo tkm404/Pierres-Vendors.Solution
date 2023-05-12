c2fe3c3b8a688795f65ce3bfa2f2f324c11c10aa
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-12 13:44:18 -0700

    add Vendor route path to Show all vendors and associated Orders.

--- Pierres-Vendors/Controllers/VendorsController.cs
+++ Pierres-Vendors/Controllers/VendorsController.cs
@@ -13,5 +13,16 @@ namespace PierresVendors.Controllers
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
   }
 }
